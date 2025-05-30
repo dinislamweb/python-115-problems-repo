2f404c41e5b32a01dde497759c7f56d513404351
dinislamweb <96992849+dinislamweb@users.noreply.github.com>
2025-05-30 22:45:11 +0600

    string reverse function added

---
+++ reverse_string.py
@@ -0,0 +1,6 @@
+#3. String Reverse: Write a Python function to reverse a given string and return the reversed string.
+def reverse_string(s):
+    """Here the parameter s that takes a string and returns the reversed string
+    """
+    return s[::-1]
+print(reverse_string("Markram"))
