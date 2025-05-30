5e2cec03f82b5bc1497dbcabd6957ac24133813e
dinislamweb <96992849+dinislamweb@users.noreply.github.com>
2025-05-30 23:12:02 +0600

    type conversion added

---
+++ type_conversion.py
@@ -0,0 +1,4 @@
+#4. Type Conversion: Given a list of integers, write a Python program to convert each element of the list to a string.
+list_of_integers = [10,20,50,100]
+list_of_strings = [str(num) for num in list_of_integers] # ["10","20","50","100"]
+print(list_of_strings)
