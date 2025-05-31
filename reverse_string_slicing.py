8b2b2896282bd61a6c23e941143bf618c14b94bc
dinislamweb <96992849+dinislamweb@users.noreply.github.com>
2025-05-31 08:41:41 +0600

    reverse string using slicing added

---
+++ reverse_string_slicing.py
@@ -0,0 +1,4 @@
+# 8. String Reversal with Slicing: Write a Python function to reverse a given string using slicing.
+def reverse_string_slicing(s):
+    return s[::-1]
+print(reverse_string_slicing("din islam"))
