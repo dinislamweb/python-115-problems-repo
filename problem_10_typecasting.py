3b6ad0a045f375b9916f07a8ef1f6d9834c257a9
dinislamweb <96992849+dinislamweb@users.noreply.github.com>
2025-05-31 09:19:47 +0600

    typecasting challenge added

---
+++ problem_10_typecasting.py
@@ -0,0 +1,13 @@
+# 10. Typecasting Challenge: Given three variables: `a = ‘100’`, `b = 25`, and `c = ‘10.5’`,
+#write a Python program to perform the following operations and print the results: 
+#– Convert `a` to an integer and add it to `b`. – Convert `c` to a float and subtract it from the result of the first operation. 
+# – Convert the final result to a string and concatenate it with the string ” is the answer.”
+def typecasting_challenge():
+    a = '100'
+    b = 25
+    c = '10.5'
+    result1 = int(a) + b 
+    result2 = result1 - float(c)
+    final_result = str(result2) + " is the answer"
+    return final_result
+print(typecasting_challenge())
